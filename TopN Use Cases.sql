574520a350884aeeb3d797389ddd5faa42c67c2a
erdebashishsingh <er.debashishsingh@users.noreply.github.com>
2020-04-29 21:47:18 -0400

    12

--- TopN Use Cases.sql
+++ TopN Use Cases.sql
@@ -1,3 +1,8 @@
 select rank() over (order by salary desc) , salary ,department from staff fetch first 1 rows only;
 
 select * from (select rank() over (partition by department order by salary desc) rnk , salary ,department from staff) where rnk = 1  ;
+
+
+
+select * from (select rank() over (order by salary desc) rnk , salary ,department from staff) where rnk = 1  ;
+select rank() over (order by salary desc) rnk , salary ,department from staff;
